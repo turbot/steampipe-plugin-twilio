0e1950262f060387ee00970a27da4eef69d0231d
misraved <ved@turbot.com>
2022-04-20 16:58:24 +0530

    Update SDK

--- twilio/table_twilio_account_message.go
+++ twilio/table_twilio_account_message.go
@@ -5,9 +5,9 @@ import (
 
 	openapi "github.com/twilio/twilio-go/rest/api/v2010"
 
-	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
-	"github.com/turbot/steampipe-plugin-sdk/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/plugin/transform"
+	"github.com/turbot/steampipe-plugin-sdk/v3/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/v3/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v3/plugin/transform"
 )
 
 //// TABLE DEFINITION
