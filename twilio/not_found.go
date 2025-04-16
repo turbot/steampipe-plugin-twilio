79a0d78b3c71fadc3c8803f0a8f325d87530a8f2
Ved misra <47312748+misraved@users.noreply.github.com>
2025-04-16 22:05:30 +0530

    Recompile plugin with steampipe-plugin-sdk v5.11.5 and Go version 1.23.1 (#57)

--- twilio/not_found.go
+++ twilio/not_found.go
@@ -1,7 +1,8 @@
 package twilio
 
 import (
-	"github.com/turbot/go-kit/helpers"
+	"slices"
+
 	"github.com/turbot/go-kit/types"
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
 	twilioclient "github.com/twilio/twilio-go/client"
@@ -11,7 +12,7 @@ import (
 func isNotFoundError(notFoundErrors []string) plugin.ErrorPredicate {
 	return func(err error) bool {
 		if twilioErr, ok := err.(*twilioclient.TwilioRestError); ok {
-			return helpers.StringSliceContains(notFoundErrors, types.ToString(twilioErr.Status))
+			return slices.Contains(notFoundErrors, types.ToString(twilioErr.Status))
 		}
 		return false
 	}
